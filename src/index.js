9cf7202ccf27194416d648b647c9e3663a7a464d
Elliot Coyle <elliot+github@vaetek.biz>
2016-09-23 13:32:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export default function wrap(existingConsole) {
+function makeConsoleIESafe(existingConsole) {
+  if (Function.prototype.bind && existingConsole && typeof existingConsole.log == "object") {
+    var existingConsoleClone = Object.assign({}, existingConsole);
+    
+    ["log","info","warn","error","assert","dir","clear","profile","profileEnd"].forEach((method) => {
+        existingConsoleClone[method] = this.bind(existingConsoleClone[method], existingConsoleClone);
+    }, Function.prototype.call);
+    
+    return existingConsoleClone;
+  }
   return existingConsole;
+}
+
+export default function wrap(existingConsole) {
+  var existingConsoleClone = makeConsoleIESafe(Object.assign({}, existingConsole));
 };
