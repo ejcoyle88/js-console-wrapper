cb2901c773381a5893d6dab7efcb40cbcf30193e
Elliot Coyle <elliotcoyle@ekmsystems.co.uk>
2016-09-23 15:48:39 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,76 @@
-function makeConsoleIESafe(existingConsole) {
-  if (Function.prototype.bind && existingConsole && typeof existingConsole.log == "object") {
-    var existingConsoleClone = Object.assign({}, existingConsole);
-    
-    ["log","info","warn","error","assert","dir","clear","profile","profileEnd"].forEach((method) => {
-        existingConsoleClone[method] = this.bind(existingConsoleClone[method], existingConsoleClone);
-    }, Function.prototype.call);
-    
-    return existingConsoleClone;
+import { EventEmitter } from 'events';
+
+function createConstants() {
+  let constants = {};
+  let argsLen = arguments.length;
+  for (let i = 0; i < argsLen; i++) {
+    let constant = arguments[i];
+    constants[constant] = constant;
   }
-  return existingConsole;
+  return constants;
 }
 
-export default function wrap(existingConsole) {
-  var existingConsoleClone = makeConsoleIESafe(Object.assign({}, existingConsole));
-};
+function makeConsoleIESafe(existingConsole) {
+  if (!Function.prototype.bind || !existingConsole || !(typeof existingConsole.log === 'object')) {
+    return existingConsole;
+  }
+
+  var existingConsoleClone = Object.assign({}, existingConsole);
+
+  ['log', 'info', 'warn', 'error', 'assert', 'dir', 'clear', 'profile', 'profileEnd'].forEach(function (method) {
+    existingConsoleClone[method] = this.bind(existingConsoleClone[method], existingConsoleClone);
+  }, Function.prototype.call);
+
+  return existingConsoleClone;
+}
+
+function wrapperFunc(emitter, passThrough, event, defaultFn) {
+  let args = Array.prototype.slice.call(arguments);
+  args = args.slice(4);
+  emitter.emit(event, args);
+  if (passThrough) {
+    defaultFn.apply(this, args);
+  }
+}
+
+export default class ConsoleWrapper {
+  static _defaultOptions = {
+    // If true, perform the normal behavior of the function as well as
+    // calling any attached callbacks.
+    passThrough: true
+  };
+  static events = createConstants(
+      'log', 'info', 'warn', 'error', 'assert',
+      'dir', 'clear', 'profile', 'profileEnd'
+  );
+
+  constructor(options) {
+    this._options = Object.assign({}, ConsoleWrapper._defaultOptions, options);
+    this._emitter = new EventEmitter();
+    this._wrapperFn = wrapperFunc.bind(this, this._emitter, this._options.passThrough);
+    this._eventLog = [];
+  }
+
+  getEventLog = () => this._eventLog;
+
+  wrap = (existingConsole) => {
+    let consoleClone = makeConsoleIESafe(existingConsole);
+    let newConsole = {};
+    for (let event in ConsoleWrapper.events) {
+      if(ConsoleWrapper.events.hasOwnProperty(event)) {
+        newConsole[event] = this._wrapperFn.bind(consoleClone, event, consoleClone[event]);
+        this._emitter.on(event, (p) => this._eventLog.push({ ...p }));
+      }
+    }
+    return newConsole;
+  };
+
+  on = (event, cb) => {
+    return this._emitter.on(event, cb);
+  };
+
+  removeListener = (event, cb) => {
+    return this._emitter.removeListener(event, cb);
+  };
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './dist',
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  }
+};
