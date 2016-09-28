7683cc6a486aece06022ef71a580b61155566ed8
Elliot Coyle <elliotcoyle@ekmsystems.co.uk>
2016-09-28 12:13:14 +0100

    Stuff

--- src/index.js
+++ src/index.js
@@ -24,34 +24,51 @@ function makeConsoleIESafe(events, existingConsole) {
   return existingConsoleClone;
 }
 
-function wrapperFunc(eventLog, emitter, passThrough, defaultFn, event) {
-  let args = Array.prototype.slice.call(arguments);
-  args = args.slice(5);
-  emitter.emit(event, args);
-  eventLog.push({ EventName: event, ...args });
-  if (passThrough) {
-    defaultFn.apply(this, args);
-  }
-}
-
 export default class ConsoleWrapper {
+  static logLevels = {
+    log: 0,
+    info: 1,
+    warn: 2,
+    error: 3
+  };
   static _defaultOptions = {
     // If true, perform the normal behavior of the function as well as
     // calling any attached callbacks.
-    passThrough: true
+    passThrough: true,
+    logLevel: ConsoleWrapper.logLevels.log
   };
   static _events = [
     'log', 'info', 'warn', 'error', 'assert',
     'dir', 'clear', 'profile', 'profileEnd'
   ];
-  static events = createConstants.apply(this, ConsoleWrapper._events);
+  static events = createConstants.apply(ConsoleWrapper, ConsoleWrapper._events);
 
   constructor(options) {
     this._options = Object.assign({}, ConsoleWrapper._defaultOptions, options);
+    this._logLevel = this._options.logLevel;
     this._eventLog = [];
     this._emitter = new EventEmitter();
-    this._wrapperFn = wrapperFunc.bind(this, this._eventLog, this._emitter, this._options.passThrough);
-    this._emit = this._wrapperFn.bind(this, function(){});
+    this._emit = this.wrapperFunc.bind(this, function(){});
+  }
+
+  getEventLogLevel(event) {
+    return ConsoleWrapper.logLevels[event];
+  }
+
+  wrapperFunc(defaultFn, event) {
+    let args = Array.prototype.slice.call(arguments);
+    args = [event, ...args.slice(2)];
+    args.logLevel = this._logLevel;
+    this._emitter.emit.apply(this._emitter, args);
+    this._eventLog.push({ EventName: event, LogLevel: this._logLevel, ...args });
+    var eventLogLevel = this.getEventLogLevel(event);
+    if (this._options.passThrough && (eventLogLevel == null || eventLogLevel >= this._logLevel)) {
+      defaultFn.apply(this, args);
+    }
+  }
+
+  setLogLevel(logLevel) {
+    this._logLevel = logLevel;
   }
 
   getEventLog = () => {
@@ -74,7 +91,7 @@ export default class ConsoleWrapper {
     };
     for (let event in ConsoleWrapper.events) {
       if(ConsoleWrapper.events.hasOwnProperty(event)) {
-        newConsole[event] = this._wrapperFn.bind(consoleClone, consoleClone[event], event);
+        newConsole[event] = this.wrapperFunc.bind(this, consoleClone[event], event);
       }
     }
     return newConsole;
